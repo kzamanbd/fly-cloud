7f7c88ae97e6fafd04a32c325ddd33b92ed439a0
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-08-08 13:33:57 +0600

    feat: added site seeder

---
+++ apps/dns-portal/database/factories/SiteFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Site>
+ */
+class SiteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->domainWord,
+            'domain' => $this->faker->domainName,
+            'ip_address' => $this->faker->ipv4,
+            'port' => $this->faker->numberBetween(1, 65535),
+            'username' => $this->faker->userName,
+        ];
+    }
+}
